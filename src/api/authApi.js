acc645db648feea7bbc6a796f7d63c3483be0843
mohammed5212 <mohammedchpktr@gmail.com>
2025-10-13 19:13:31 +0530

    modified auth

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,23 +1,25 @@
 import axios from "axios";
-const API_URL ="https://e-commerce-wncj.onrender.com/auth"
 
-//login
-export const loginUser =async(Credentials)=>{
-    console.log("Logged in user:", user);
-    try{
-        const res =await axios.post(`${API_URL}/login`,Credentials)
-        return res.data  //should {token ,data}
+const API_URL = "https://e-commerce-wncj.onrender.com/api/auth"; 
 
-    }catch(error){
-        throw error.response?.data || {message:"Login failed"}}
-}
-//register
+// login
+export const loginUser = async (credentials) => {
+  try {
+    const res = await axios.post(`${API_URL}/login`, credentials);
+    return res.data; // should return { token, data }
+  } catch (error) {
+    throw error.response?.data || { message: "Login failed" };
+  }
+};
 
-export const registerUser= async(userData)=>{
-    try{
-        const res =await axios.post ("https://e-commerce-wncj.onrender.com/api/auth/register/",userData)
-        return res.data    //could (message user) or {token}
-    }catch(error){
-        throw error.response?.data || { message: "Network error or server unreachable." }
-    }
-}
+// register
+export const registerUser = async (userData) => {
+  try {
+    const res = await axios.post(`${API_URL}/register`, userData);
+    return res.data; // could { message, user } or { token }
+  } catch (error) {
+    throw error.response?.data || {
+      message: "Network error or server unreachable.",
+    };
+  }
+};
