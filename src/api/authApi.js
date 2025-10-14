c1af38c6f62a7a032e606d005f47318d1a498161
mohammed5212 <mohammedchpktr@gmail.com>
2025-10-14 23:32:14 +0530

    modified authapi

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -5,7 +5,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 // Login user
 export const loginUser = async (credentials) => {
   try {
-    const res = await axios.post(`${API_URL}/login`, credentials);
+    const res = await axios.post(`${API_URL}/auth/login`, credentials);
     return res.data; // expected { token, data }
   } catch (error) {
     throw error.response?.data || { message: "Login failed" };
@@ -15,7 +15,7 @@ export const loginUser = async (credentials) => {
 // Register user
 export const registerUser = async (userData) => {
   try {
-    const res = await axios.post(`${API_URL}/register`, userData);
+    const res = await axios.post(`${API_URL}/auth/register`, userData);
     return res.data; // could be { message, user } or { token }
   } catch (error) {
     throw error.response?.data || {
