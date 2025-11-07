482618844604099f485e7455ac359d8d6bc461b4
mohammed5212 <mohammedchpktr@gmail.com>
2025-11-07 19:59:47 +0530

    y

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "https://e-commerce-vdkn.onrender.com/api";
+const API_URL = import.meta.env.VITE_API_URL || "https://e-commerce-backend-1-btb6.onrender.com/api";
 // Login user
 export const loginUser = async (credentials) => {
   try {
